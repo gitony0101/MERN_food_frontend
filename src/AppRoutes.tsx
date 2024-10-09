910c11e28489267fd504d4ace1964149101f88fc
gitony0101 <53283254+gitony0101@users.noreply.github.com>
2024-10-08 22:02:05 -0300

    user profilepage works full
    Signed-off-by: gitony0101 <53283254+gitony0101@users.noreply.github.com>

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -2,6 +2,7 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import Layout from './layouts/layout';
 import AuthCallbackPage from './pages/AuthCallBackPage';
+import UserProfilePage from './pages/UserProfilePage';
 const AppRoutes = () => {
   return (
     <Routes>
@@ -14,7 +15,14 @@ const AppRoutes = () => {
         }
       />
       <Route path="/auth-callback" element={<AuthCallbackPage />} />
-      <Route path="/user-profile" element={<span>USER PROFILE PAGE</span>} />
+      <Route
+        path="/user-profile"
+        element={
+          <Layout>
+            <UserProfilePage />
+          </Layout>
+        }
+      />
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   );
