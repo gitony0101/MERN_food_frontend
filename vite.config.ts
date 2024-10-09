5d808b0051fdc19adc789079060b28a92ab1a8c7
gitony0101 <53283254+gitony0101@users.noreply.github.com>
2024-10-09 14:03:02 -0300

    vite config
    Signed-off-by: gitony0101 <53283254+gitony0101@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'), // 这里将 @ 映射到 ./src 目录
-    },
-  },
 });
