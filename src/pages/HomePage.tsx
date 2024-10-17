75bc790641b576b001bf98ca6dfb37da62ef53be
gitony0101 <53283254+gitony0101@users.noreply.github.com>
2024-10-16 21:48:21 -0300

    searchpage ing
    Signed-off-by: gitony0101 <53283254+gitony0101@users.noreply.github.com>

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,27 @@
 import landingImage from '../assets/landing.png';
 import appDownloadImage from '../assets/appDownload.png';
+import SearchBar, { SearchForm } from '../components/SearchBar';
+import { useNavigate } from 'react-router-dom';
 
 export default function HomePage() {
+  const navigate = useNavigate();
+  const handleSearchSubmit = (searchFromValues: SearchForm) => {
+    navigate({
+      pathname: `/search/${searchFromValues.searchQuery}`,
+    });
+  };
   return (
     <div className="flex flex-col gap-12">
       <div>
-        <div className="bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
+        <div className="md:px-32 bg-white rounded-lg shadow-md py-8 flex flex-col gap-5 text-center -mt-16">
           <h1 className="text-5xl font-bold tracking-tight text-orange-6000">
             Tuck into takeaway today
           </h1>
           <span className="text-xl">Food is just a click away!</span>
+          <SearchBar
+            placeHolder="Search by city or town."
+            onSubmit={handleSearchSubmit}
+          />
         </div>
       </div>
       <div className="grid md:grid-cols-2 gap-5">
---
+++ src/pages/SearchPage.tsx
@@ -0,0 +1,6 @@
+import { useParams } from 'react-router-dom';
+
+export default function SearchPage() {
+  const { city } = useParams();
+  return <span>User searched for {city}</span>;
+}
