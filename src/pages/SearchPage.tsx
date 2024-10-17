7137e9f741cd867033d50a19fb294dee935b7f55
gitony0101 <53283254+gitony0101@users.noreply.github.com>
2024-10-17 06:56:10 -0300

    fully works:User searched for Halifax found - halifa res,Halifax
    Signed-off-by: gitony0101 <53283254+gitony0101@users.noreply.github.com>

--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -1,6 +1,21 @@
 import { useParams } from 'react-router-dom';
+import { useSearchRestaurants } from '../api/restaurant/RestaurantApi';
 
 export default function SearchPage() {
   const { city } = useParams();
-  return <span>User searched for {city}</span>;
+  const { results } = useSearchRestaurants(city);
+
+  return (
+    <span>
+      User searched for {city}
+      {'  '}
+      <span>
+        {results?.data.map((restaurant) => (
+          <span>
+            found - {restaurant.restaurantName},{restaurant.city}
+          </span>
+        ))}
+      </span>
+    </span>
+  );
 }
