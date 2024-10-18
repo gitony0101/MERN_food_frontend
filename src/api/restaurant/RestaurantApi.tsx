909e3f8374f4fd25e11a4e70365b47751cbd71d9
gitony0101 <53283254+gitony0101@users.noreply.github.com>
2024-10-18 07:41:58 -0300

    10:23:14 working update
    Signed-off-by: gitony0101 <53283254+gitony0101@users.noreply.github.com>

--- src/api/restaurant/RestaurantApi.tsx
+++ src/api/restaurant/RestaurantApi.tsx
@@ -1,10 +1,16 @@
 import { useQuery } from 'react-query';
 import { RestaurantSearchResponse } from '../../types';
+import type { SearchState } from '../../pages/SearchPage';
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
-export const useSearchRestaurants = (city?: String) => {
+export const useSearchRestaurants = (
+  searchState: SearchState,
+  city?: String,
+) => {
   const createSearchRequest = async (): Promise<RestaurantSearchResponse> => {
+    const params = new URLSearchParams();
+    params.set('searchQuery', searchState.searchQuery);
     const response = await fetch(
       `${API_BASE_URL}/api/restaurant/search/${city}`,
     );
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -2,10 +2,34 @@ import { useParams } from 'react-router-dom';
 import { useSearchRestaurants } from '../api/restaurant/RestaurantApi';
 import SearchResultInfo from '../components/SearchResultInfo';
 import SearchResultCard from '../components/SearchResultCard';
+import { useState } from 'react';
+import SearchBar, { type SearchForm } from '../components/SearchBar';
+
+export type SearchState = {
+  searchQuery: string;
+};
 
 export default function SearchPage() {
   const { city } = useParams();
-  const { results, isLoading } = useSearchRestaurants(city);
+  const [searchState, setSearchState] = useState<SearchState>({
+    searchQuery: '',
+  });
+  const { results, isLoading } = useSearchRestaurants(searchState, city);
+
+  const setSearchQuery = (searchFromData: SearchForm) => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      searchQuery: searchFromData.searchQuery,
+    }));
+  };
+
+  const resetSearch = () => {
+    setSearchState((prevState) => ({
+      ...prevState,
+      searchQuery: '',
+    }));
+  };
+
   if (isLoading) {
     <span>Loading...</span>;
   }
@@ -18,22 +42,17 @@ export default function SearchPage() {
     <div className="grid grid-cols-1 lg:grid-cols-[205px_1fr] gap-5">
       <div id="cuisines-list">insert cuisines here.</div>
       <div id="main-content" className="flex flex-col gap-5">
+        <SearchBar
+          searchQuery={searchState.searchQuery}
+          onSubmit={setSearchQuery}
+          placeHolder="Search by Cuisine or Restaurant Name"
+          onReset={resetSearch}
+        />
         <SearchResultInfo total={results.pagination.total} city={city} />
         {results.data.map((restaurant) => (
           <SearchResultCard restaurant={restaurant} />
         ))}
       </div>
     </div>
-
-    // <span>
-    //   User searched for {city}{' '}
-    //   <span>
-    //     {results?.data.map((restaurant) => (
-    //       <span>
-    //         found - {restaurant.restaurantName},{restaurant.city}
-    //       </span>
-    //     ))}
-    //   </span>
-    // </span>
   );
 }
