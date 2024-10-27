35066b860c124b8db4d2922a2b0a5f4320640670
gitony0101 <53283254+gitony0101@users.noreply.github.com>
2024-10-26 21:53:57 -0300

    update
    Signed-off-by: gitony0101 <53283254+gitony0101@users.noreply.github.com>

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -8,6 +8,7 @@ import ManageRestaurantPage from './pages/ManageRestaurantPage';
 import SearchPage from './pages/SearchPage';
 import DetailPage from './pages/DetailPage';
 import OrderStatusPage from './pages/OrderStatusPage';
+
 const AppRoutes = () => {
   return (
     <Routes>
